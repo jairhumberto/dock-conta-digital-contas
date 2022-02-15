76d95fc02d6be84f73cd89f44c14bc7df978f41c
Humberto <jairhumberto@gmail.com>
2022-02-15 16:02:41 -0300

    Adicao das annotations para validacao dos dados

--- Models/Conta.cs
+++ Models/Conta.cs
@@ -1,13 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContasService.Models
 {
     public class Conta
     {
+        [Key, Required]
         public int Id { get; set; }
+
+        [Required]
         public string Numero { get; set; }
+
+        [Required]
         public string Agencia { get; set; }
+
+        [Required]
         public decimal Saldo { get; set; }
+
+        [Required]
         public string PortadorCpf { get; set; }
+
+        [Required]
         public bool Ativa { get; set; }
+
+        [Required]
         public bool Bloqueada { get; set; }
     }
 }
