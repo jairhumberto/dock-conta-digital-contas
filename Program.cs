13de697b4e5d79d0cac41202a6e02078e7a69eee
Humberto <jairhumberto@gmail.com>
2022-02-15 23:29:16 -0300

    Inclusao de configuracao para habilitacao de Http Patch

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using ContasService.Data;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
 builder.Services.AddScoped<IContasRepository, ContasRepository>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(s => {
+    s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+});
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
